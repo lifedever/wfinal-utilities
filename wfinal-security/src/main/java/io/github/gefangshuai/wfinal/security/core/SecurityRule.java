8e323c6ff1d8a6d4f0c775ea1faad6eb67fd250b
葛方帅 <gefangshuai@163.com>
2015-08-18 11:19:42 +0800

    完成整个Security扩展

--- wfinal-security/src/main/java/io/github/gefangshuai/wfinal/security/core/SecurityRule.java
+++ wfinal-security/src/main/java/io/github/gefangshuai/wfinal/security/core/SecurityRule.java
@@ -1,31 +1,45 @@
 package io.github.gefangshuai.wfinal.security.core;
 
+import com.jfinal.core.Controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 安全防护规则, 设置拦截的url，用于全局的配置。跟注解并行，注解优先级高于此配置
  * Created by gefangshuai on 2015/7/27.
  */
 public class SecurityRule {
-    private String[] filterUrls;    // 要过滤的url
+    private String[] loginFilterUrls;    // 要过滤的url
     private String loginUrl;    // 登录url
     private boolean backToLoginPage = false;    // 登录失败是否跳转回登陆页面
     private boolean useAccessActionFilter = false;  // 是否启用用户请求拦截
+    private Map<String, Class<? extends Controller>[]> accessPermissionMap = new HashMap<>();
     private String subjectKey = SecurityConst.SECURITY_SESSION_SUBJECT_KEY;     // 前台获取登录信息的key
 
     public SecurityRule() {
     }
 
-    public String[] getFilterUrls() {
-        return filterUrls;
+    public String[] getLoginFilterUrls() {
+        return loginFilterUrls;
     }
 
-    public void setFilterUrls(String[] filterUrls) {
-        this.filterUrls = filterUrls;
+    /**
+     * 设置要过滤的url
+     * @param loginFilterUrls
+     */
+    public void setLoginFilterUrls(String[] loginFilterUrls) {
+        this.loginFilterUrls = loginFilterUrls;
     }
 
     public String getLoginUrl() {
         return loginUrl;
     }
 
+    /**
+     * 设置登录页面地址
+     * @param loginUrl
+     */
     public void setLoginUrl(String loginUrl) {
         this.loginUrl = loginUrl;
     }
@@ -34,6 +48,10 @@ public class SecurityRule {
         return backToLoginPage;
     }
 
+    /**
+     * 校验失败是否跳转回登录页面
+     * @param backToLoginPage
+     */
     public void setBackToLoginPage(boolean backToLoginPage) {
         this.backToLoginPage = backToLoginPage;
     }
@@ -42,6 +60,10 @@ public class SecurityRule {
         return subjectKey;
     }
 
+    /**
+     * 设置Subject对应的key，默认为 <code>SecurityConst.SECURITY_SESSION_SUBJECT_KEY</code>
+     * @param subjectKey
+     */
     public void setSubjectKey(String subjectKey) {
         this.subjectKey = subjectKey;
     }
@@ -50,7 +72,23 @@ public class SecurityRule {
         return useAccessActionFilter;
     }
 
+    /**
+     * 是否开启更细的权限访问
+     * @param useAccessActionFilter
+     */
     public void setUseAccessActionFilter(boolean useAccessActionFilter) {
         this.useAccessActionFilter = useAccessActionFilter;
+    }
+
+    public Map<String, Class<? extends Controller>[]> getAccessPermissionMap() {
+        return accessPermissionMap;
+    }
+
+    /**
+     * 特定权限对应的请求地址前缀，如: <code>accessPermissionMap.put("admin", new Controller[]{"/user","/admin"})</code>
+     * @param accessPermissionMap
+     */
+    public void setAccessPermissionMap(Map<String, Class<? extends Controller>[]> accessPermissionMap) {
+        this.accessPermissionMap = accessPermissionMap;
     }
 }
--- wfinal-security/src/main/java/io/github/gefangshuai/wfinal/security/interceptor/SecurityInterceptor.java
+++ wfinal-security/src/main/java/io/github/gefangshuai/wfinal/security/interceptor/SecurityInterceptor.java
@@ -4,6 +4,7 @@ import com.jfinal.aop.Interceptor;
 import com.jfinal.aop.Invocation;
 import com.jfinal.core.Controller;
 import com.jfinal.ext.kit.JfinalKit;
+import com.jfinal.log.Logger;
 import io.github.gefangshuai.wfinal.base.utils.SessionKit;
 import io.github.gefangshuai.wfinal.security.annotation.AccessClear;
 import io.github.gefangshuai.wfinal.security.annotation.AccessPermissions;
@@ -17,12 +18,32 @@ import io.github.gefangshuai.wfinal.security.plugin.SecurityPlugin;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * 安全拦截器
  * Created by gefangshuai on 2015/7/20.
  */
 public class SecurityInterceptor implements Interceptor {
+    Logger logger = Logger.getLogger(SecurityInterceptor.class);
+
+    private Set<Class<? extends Controller>> accessPermissionControllers;
+    private SecurityRule securityRule;
+
+    private Set<Class<? extends Controller>> getAccessPermissionControllers() {
+        if (accessPermissionControllers == null) {
+            accessPermissionControllers = new HashSet<>();
+            Map<String, Class<? extends Controller>[]> accessPermissionMap = securityRule.getAccessPermissionMap();
+            for (Class<? extends Controller>[] classes : accessPermissionMap.values()) {
+                for (Class<? extends Controller> clazz : classes) {
+                    accessPermissionControllers.add(clazz);
+                }
+            }
+        }
+        return accessPermissionControllers;
+    }
 
     @Override
     public void intercept(Invocation inv) {
@@ -30,7 +51,7 @@ public class SecurityInterceptor implements Interceptor {
         SessionKit.put(controller.getSession());
         Method method = inv.getMethod();
         SecurityPlugin securityPlugin = (SecurityPlugin) JfinalKit.findPlugin(SecurityPlugin.class).get(0);
-        SecurityRule securityRule = securityPlugin.getSecurityRule();
+        securityRule = securityPlugin.getSecurityRule();
         Subject subject = SecurityKit.getSubject(controller);
         controller.setAttr(securityRule.getSubjectKey(), subject);
 
@@ -40,7 +61,7 @@ public class SecurityInterceptor implements Interceptor {
             }
         }
 
-        if (needLoginCheck(securityRule, controller, method)) {   // 需要登录验证
+        if (needLoginCheck(controller, method)) {   // 需要登录验证
             if (hasLogged(controller)) {    // 登录成功
                 clearUrlBeforeLoginInfo(controller);
                 inv.invoke();
@@ -63,13 +84,14 @@ public class SecurityInterceptor implements Interceptor {
     private boolean accessActionCheck(Invocation inv) {
         Controller controller = inv.getController();
         Method method = inv.getMethod();
+
         Subject subject = SecurityKit.getSubject(controller);
 
         // 有清除注解，直接通过校验
         if (method.isAnnotationPresent(AccessClear.class) || controller.getClass().isAnnotationPresent(AccessClear.class))
             return true;
 
-        // 判断subject设置的Role是否通过校验
+        // 判断subject设置的Permission注解是否通过校验
         if (controller.getClass().isAnnotationPresent(AccessPermissions.class) || method.isAnnotationPresent(AccessPermissions.class)) {
             if (subject.getPermissions() == null)
                 return false;
@@ -83,7 +105,31 @@ public class SecurityInterceptor implements Interceptor {
             }
             return false;
         }
+
+        // 当前登录用户是否有全局 AccessPermissionMap 的权限
+        if (isThisControllerInAccessPermissions(controller.getClass())) {
+            Map<String, Class<? extends Controller>[]> accessPermissionMap = securityRule.getAccessPermissionMap();
+
+            for (String permission : subject.getPermissions()) {
+                if (accessPermissionMap.containsKey(permission)) {
+                    Class<? extends Controller>[] classes = accessPermissionMap.get(permission);
+                    if (Arrays.asList(classes).contains(controller.getClass()))
+                        return true;
+                }
+            }
+            return false;
+        }
+
         return true;
+    }
+
+    /**
+     * 判断当前的Contoller是否被定义在全局的控制列表中
+     */
+    private boolean isThisControllerInAccessPermissions(Class<? extends Controller> clazz) {
+        if (getAccessPermissionControllers().contains(clazz))
+            return true;
+        return false;
     }
 
     /**
@@ -93,9 +139,9 @@ public class SecurityInterceptor implements Interceptor {
      * @param method
      * @return
      */
-    private boolean needLoginCheck(SecurityRule securityRule, Controller controller, Method method) {
+    private boolean needLoginCheck(Controller controller, Method method) {
         boolean ruleConf = false;
-        for (String url : securityRule.getFilterUrls()) {
+        for (String url : securityRule.getLoginFilterUrls()) {
             url = url.endsWith("/") ? url : url + "/";
             String servletPath = controller.getRequest().getServletPath();
             servletPath = servletPath.endsWith("/") ? servletPath : servletPath + "/";
